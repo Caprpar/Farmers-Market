d2256f0d74627e6864feba0025c0a3b64b14da5f
caspar <cappetjatte@gmail.com>
2025-12-09 11:09:22 +0100

    add init.sql and structure database

--- backend/init.sql
+++ backend/init.sql
@@ -53,112 +53,113 @@ VALUES
 
 -- add sessions 
 INSERT INTO 
-  session(
-    current_balance,
-    top_amount,
-    biggest_loss,
-    biggest_win,
-    highest_bet
-  )
+session(
+  current_balance,
+  top_amount,
+  biggest_loss,
+  biggest_win,
+  highest_bet
+)
 VALUES
   (4222, 7000, 500, 2000, 6000),
   (6422, 1230, 4021, 1200, 1700),
   (1239, 5422, 100, 499, 1090),
   (554, 4992, 2340, 900, 1000);
 
---- add sessions to player
-  INSERT INTO 
-    player_session(
-    player_id, session_id
-  )
-  VALUES
-    (1,1),
-    (1,2),
-    (1,3),
-    (1,4);
-- add sessions to player
-  INSERT INTO 
-    player_session(
-    player_id, session_id
-  )
-  VALUES
-    (1,1),
-    (1,2),
-    (1,3),
-    (1,4);
+-- add sessions to player
+INSERT INTO 
+  player_session(
+  player_id, session_id
+)
+VALUES
+  (1,1),
+  (1,2),
+  (1,3),
+  (1,4);
+
+-- add sessions to player
+INSERT INTO 
+  player_session(
+  player_id, session_id
+)
+VALUES
+  (1,1),
+  (1,2),
+  (1,3),
+  (1,4);
 
 -- add player
-  INSERT INTO 
-    player(name, password_hash, current_balance, highest_score)
-  VALUES (
-    $1,
-    $2,
-    $3,
-    $4
-  );
+INSERT INTO 
+  player(name, password_hash, current_balance, highest_score)
+VALUES (
+  $1,
+  $2,
+  $3,
+  $4
+);
 
 -- add session
-  INSERT INTO 
-    session(
-      current_balance,
-      top_amount,
-      biggest_loss,
-      biggest_win,
-      highest_bet
-    )
-    VALUES
-      ($1, $2, $3, $4, $5);
+INSERT INTO session(
+  current_balance,
+  top_amount,
+  biggest_loss,
+  biggest_win,
+  highest_bet,
+  player_id
+)
+VALUES
+  ($1, $2, $3, $4, $5);
 
 -- add session to player
-  INSERT INTO player_session(
-    player_id, session_id
-  )
-  VALUES
-    ($1,$2);
+INSERT INTO player_session(
+  player_id, session_id
+)
+VALUES
+  ($1,$2);
 
 -- Get sessions by player id
 SELECT name from player; 
 
 -- Get sessions by player id
-  SELECT p.name as player,
-    COUNT(s.id) as session_amount,
-    COALESCE(
-      JSON_AGG(
-        JSON_BUILD_OBJECT(
-        -- 'id', s.id,
-        'current_balance', s.current_balance,
-        'top_amount', s.top_amount,
-        'biggest_loss', s.biggest_loss,
-        'biggest_win', s.biggest_win,
-        'highest_bet', s.highest_bet
-        )
-        ORDER BY s.id
-      ) FILTER (WHERE s.id IS NOT NULL),
-    '[]'::json
-    ) AS sessions
-    FROM player p
-    LEFT JOIN player_session ps ON ps.player_id = p.id
-    LEFT JOIN session s on s.id = ps.session_id
-    WHERE p.id = $1
-    GROUP by p.name;
+SELECT p.name as player,
+  COUNT(s.id) as session_amount,
+  COALESCE(
+    JSON_AGG(
+      JSON_BUILD_OBJECT(
+      -- 'id', s.id,
+      'current_balance', s.current_balance,
+      'top_amount', s.top_amount,
+      'biggest_loss', s.biggest_loss,
+      'biggest_win', s.biggest_win,
+      'highest_bet', s.highest_bet
+      )
+      ORDER BY s.id
+    ) FILTER (WHERE s.id IS NOT NULL),
+  '[]'::json
+  ) AS sessions
+  FROM player p
+  LEFT JOIN player_session ps ON ps.player_id = p.id
+  LEFT JOIN session s on s.id = ps.session_id
+  WHERE p.id = $1
+  GROUP by p.name;
 
 -- update session data by sessionId
-  UPDATE session
-  SET current_balance = $1,
-      top_amount = $2,
-      biggest_loss = $3,
-      biggest_win = $4,
-      highest_bet = $5
-  WHERE id = $6;
+UPDATE session
+SET current_balance = $1,
+    top_amount = $2,
+    biggest_loss = $3,
+    biggest_win = $4,
+    highest_bet = $5
+WHERE id = $6;
 
 -- Get players with each highscore
-  SELECT p.name as player,
-  COALESCE(MAX(s.top_amount),0) as highscore
-  FROM player p 
-  LEFT JOIN player_session ps ON ps.player_id = p.id
-  LEFT JOIN session s ON s.player_id = ps.id
-  GROUP BY p.name;
-
+SELECT p.name as player,
+COALESCE(MAX(s.top_amount),0) as highscore
+FROM player p 
+LEFT JOIN player_session ps ON ps.player_id = p.id
+LEFT JOIN session s ON s.player_id = ps.id
+GROUP BY p.name
+ORDER BY highscore DESC;
 
 -- DROP TABLE player;
 -- DROP TABLE session;
