add7951a9ecec2724e6dfec418352538db6ecd6f
caspar <cappetjatte@gmail.com>
2025-12-15 17:13:18 +0100

    add types

---
+++ backend/src/types.ts
@@ -0,0 +1,47 @@
+export interface AllPlayerData {
+  id: string;
+  name: string;
+  password_hash: string;
+  current_balance: number;
+  highest_score: number;
+}
+
+export interface UpdateUserConfirmation {
+  user_updated: Partial<PlayerNameRow>;
+}
+
+export interface AddUserConfirmation {
+  user_added: Partial<PlayerNameRow>;
+}
+
+export interface DeleteUserConfirmation {
+  user_deleted: Partial<PlayerNameRow>;
+}
+
+export type ErrorResult = {
+  error: unknown;
+};
+
+export type Result<T> =
+  | {
+      ok: true;
+      data: T;
+    }
+  | { ok: false; error: unknown };
+
+export type PlayerNameRow = {
+  id: number;
+  name: string;
+};
+
+export type PlayerRow = {
+  id: number;
+  name: string;
+  password_hash: string;
+  current_balance: number;
+  highest_score: number;
+};
+
+export interface WithAuth {
+  token: string;
+}
