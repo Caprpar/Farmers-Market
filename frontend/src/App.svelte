10ddc0042d38b52a75389b90de88fc27931f564c
caspar <cappetjatte@gmail.com>
2025-12-17 15:51:28 +0100

    add routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import viteLogo from "/vite.svg";
-  import LoginForm from "./lib/LoginForm.svelte";
+  import Router from "svelte-spa-router";
+  import Home from "./routes/Home.svelte";
+  import Play from "./routes/Play.svelte";
+  import Scoreboard from "./routes/Scoreboard.svelte";
+  const routes = {
+    "/": Home,
+    "/play": Play,
+    "/scoreboard": Scoreboard,
+  };
 </script>
 
-<main>
-  <LoginForm signUpForm={false} />
-</main>
+<div class="absolute top-1 left-1">
+  <ul class="flex gap-2">
+    <ol><a href="#/">Login</a></ol>
+    <ol><a href="#/play">Play</a></ol>
+    <ol><a href="#/scoreboard">Scoreboard</a></ol>
+  </ul>
+</div>
+<Router {routes} />
