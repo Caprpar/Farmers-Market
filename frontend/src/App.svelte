ceccbdc29ae2e0657990f083c81f8620b730bd79
caspar <cappetjatte@gmail.com>
2025-12-17 18:06:02 +0100

    add logout and reset balance to navbar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
+  import { push } from "svelte-spa-router";
   import Home from "./routes/Home.svelte";
   import Play from "./routes/Play.svelte";
   import Scoreboard from "./routes/Scoreboard.svelte";
@@ -11,10 +12,34 @@
 </script>
 
 <div class="absolute top-1 left-1">
-  <ul class="flex gap-2">
+  <ul class="flex gap-3">
     <ol><a href="#/">Login</a></ol>
+    <ol>
+      <button
+        class="text-amber-700 font-bold cursor-pointer hover:text-amber-800"
+        onclick={() => {
+          localStorage.removeItem("auth_token");
+          push("/");
+        }}>LogOut</button
+      >
+    </ol>
     <ol><a href="#/play">Play</a></ol>
     <ol><a href="#/scoreboard">Scoreboard</a></ol>
+    <ol>
+      <button
+        class="text-amber-700 font-bold cursor-pointer hover:text-amber-800"
+        onclick={async () => {
+          const token: string | null = localStorage.getItem("auth_token");
+          const headers = new Headers({ "Content-Type": "application/json" });
+          if (token) headers.set("authorization", token);
+          const res = await fetch(`http://localhost:3000/api/player/0`, {
+            method: "PATCH",
+            headers,
+            body: JSON.stringify({ current_balance: 1000 }),
+          });
+        }}>Reset balance</button
+      >
+    </ol>
   </ul>
 </div>
 <Router {routes} />
