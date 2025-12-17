e34348ed67c053c4ac203f39a5ef97297d262e25
caspar <cappetjatte@gmail.com>
2025-12-17 15:50:11 +0100

    minor fixes for tests

--- frontend/cypress/component/Highscore.cy.ts
+++ frontend/cypress/component/Highscore.cy.ts
@@ -1,43 +1,45 @@
 import { mount } from "cypress/svelte";
 import Highscore from "../../src/lib/Highscore.svelte";
+
 describe("Highscore.cy.ts", () => {
-  cy.intercept("GET", "/api/Highscore", {
-    body: {
-      players: [
+  beforeEach(() => {
+    cy.intercept("GET", "/api/scoreboard", {
+      body: [
         {
-          name: "Caspar",
-          score: 4000,
+          player: "Caspar",
+          highscore: 4000,
         },
         {
-          name: "Felix",
-          score: 3000,
+          player: "Felix",
+          highscore: 3000,
         },
         {
-          name: "Hampus",
-          score: 2000,
+          player: "Hampus",
+          highscore: 2000,
         },
         {
-          name: "Ingo",
-          score: 1000,
+          player: "Ingo",
+          highscore: 1000,
         },
       ],
-    },
-  }).as("getHighscore");
+    }).as("getHighscore");
+  });
 
   it("Displays user score 1 if Caspar", () => {
     mount(Highscore, { props: { currentUser: "Caspar" } });
-    cy.wait("@getAllPlayers");
-    cy.get('data-cy="scoreboard"').should("contain.text", "1");
+    cy.wait("@getHighscore");
+    cy.get('[data-cy="scoreboard"]').should("contain.text", "1");
   });
 
   it("Displays user score 4 if Ingo", () => {
     mount(Highscore, { props: { currentUser: "Ingo" } });
-    cy.wait("@getAllPlayers");
-    cy.get('data-cy="scoreboard"').should("contain.text", "4");
+    cy.wait("@getHighscore");
+    cy.get('[data-cy="scoreboard"]').should("contain.text", "4");
   });
 
   it("renders a table with 4 rows, containing the right data", () => {
     mount(Highscore);
+    cy.wait("@getHighscore");
     cy.get('[data-cy="name-1"]').should("contain.text", "Caspar");
     cy.get('[data-cy="score-1"]').should("contain.text", "4000");
     cy.get('[data-cy="name-2"]').should("contain.text", "Felix");
@@ -46,6 +48,5 @@ describe("Highscore.cy.ts", () => {
     cy.get('[data-cy="score-3"]').should("contain.text", "2000");
     cy.get('[data-cy="name-4"]').should("contain.text", "Ingo");
     cy.get('[data-cy="score-4"]').should("contain.text", "1000");
-    cy.get('[data-cy="close-btn"]').should("exist");
   });
 });
--- frontend/cypress/component/LoginSignup.cy.ts
+++ frontend/cypress/component/LoginSignup.cy.ts
@@ -43,31 +43,28 @@ describe("LoginForm.cy.ts", () => {
   });
 
   it("When entering username that does not exist in database show error 'User or password is incorrect!' ", () => {
+    cy.intercept("POST", "**/api/player", (req) => {
+      expect(req.body).to.deep.equal({
+        name: "Caspar",
+        password: "password123",
+        confirmPassword: "password123",
+      });
+
+      req.reply({
+        statusCode: 422,
+        body: { error: "User already exists, please enter another username" },
+      });
+    }).as("signup");
+
     mount(LoginForm, { props: { signUpForm: true } });
-    cy.intercept("GET", "/api/players", {
-      body: {
-        players: [
-          {
-            name: "Caspar",
-          },
-          {
-            name: "Felix",
-          },
-          {
-            name: "Hampus",
-          },
-          {
-            name: "Ingo",
-          },
-        ],
-      },
-    }).as("getAllPlayers");
-    cy.wait("@getAllPlayers");
-    cy.get('[data-cy="username-input"]').type("Caprpar");
+    cy.get('[data-cy="username-input"]').type("Caspar");
     cy.get('[data-cy="password-input"]').type("password123");
+    cy.get('[data-cy="confirm-password-input"]').type("password123");
+    cy.get('[data-cy="confirm-btn"]').click();
+    cy.wait("@signup");
     cy.get('[data-cy="login-error"]').should(
       "have.text",
-      "User or password is incorrect!",
+      "User already exists, please enter another username",
     );
   });
 });
